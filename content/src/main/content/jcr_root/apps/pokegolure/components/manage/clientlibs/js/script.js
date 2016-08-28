b56fcda8bcbbc3e9a27d203555bee4ba7b3dd70e
Michael Leroy <mickjleroy@gmail.com>
2016-08-28 18:44:08 +1000

    defaulting map to Melbourne CBD

--- content/src/main/content/jcr_root/apps/pokegolure/components/manage/clientlibs/js/script.js
+++ content/src/main/content/jcr_root/apps/pokegolure/components/manage/clientlibs/js/script.js
@@ -2,11 +2,13 @@ var PokeGoLure = PokeGoLure || {}
 PokeGoLure.Manage = (function ($) {
     'use strict';
     
-    var map;
+    var map,
+        DEFAULT_LAT = -37.8150085,
+        DEFAULT_LONG = 144.9658801;
     
     function _initMap() {
         map = new google.maps.Map(document.getElementById('gmap'), {
-            center: {lat: -34.397, lng: 150.644},
+            center: {lat: DEFAULT_LAT, lng: DEFAULT_LONG},
             scrollwheel: false,
             zoom: 8,
             clickableIcons: false,
